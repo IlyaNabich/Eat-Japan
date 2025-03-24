f5f40938e0753a0f86652a8cd85b5b2a34dfb6c8
Ilya <ilnaiba@mail.ru>
2025-03-24 19:02:18 +0300

    Some edits

---
+++ Eat-Japan/DataAccess/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Eat_Japan.DataAccess
+{
+    public class AppContext(DbContextOptions<AppContext> options) : DbContext(options)
+    {
+        public DbSet<Note> Notes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>().HasKey(x => x.Id);
+            modelBuilder.Entity<Note>().Property(x => x.Id).HasMaxLength(140);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Eat-Japan/obj/Debug/net8.0/Eat-Japan.AssemblyInfo.cs
+++ Eat-Japan/obj/Debug/net8.0/Eat-Japan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eat-Japan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a7101aa84168919c2af742326550bef564a18c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eat-Japan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eat-Japan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
